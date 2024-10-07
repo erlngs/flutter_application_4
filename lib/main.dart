b1cbecfca2e6733a6627a0330f37c1aa598c8d61
Your Name <you@example.com>
2024-10-07 16:04:02 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_4/register_page.dart';
+import 'package:flutter_application_4/login_page.dart';
+
+void main (){
+  runApp( Myapp());
+}
+
+class Myapp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login/Register Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/Register' : (context) => RegisterPage(),
+      },
+    );
+  }
+}
